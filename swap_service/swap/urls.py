a96f7c39be118660580c5f04b2a9e87d37a1fa41
ISSSSA <52818700+ISSSSA@users.noreply.github.com>
2025-05-12 20:55:12 +0300

    refactored auth

--- swap_service/swap/urls.py
+++ swap_service/swap/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from .router import router
 from .views import get_csrf_token
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('api/auth/', include('rest_framework.urls')),
     path('api/csrf/', get_csrf_token, name='get_csrf'),
+    path('api/auth/login/', obtain_auth_token, name='login'),
 ]
